5c363451bc3e48599275c2b04eceb9030a0478f0
n <n@enne.tech>
2020-11-17 21:37:49 +0100

    > PREPARING FOR MAGIC

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function home()
+    {
+        return view('welcome');
+    }
+
+    public function user(Request $request) {
+        return $request->user();
+    }
 }
